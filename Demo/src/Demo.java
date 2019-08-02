d8fdf7662aadd01bb0a86c590e8f7d28a342de8c
rin1218 <kris.hyerinhyun@gmail.com>
2019-08-02 15:36:48 -0600

    Fix the external jars to run the file. Remove the external jars from the node "Modulepath". Select the node "Classpath" then add the external jars. Review that all the jars are under the node "Classpath"

--- Demo/src/Demo.java
+++ Demo/src/Demo.java
@@ -12,9 +12,9 @@ public class Demo {
 		  
 		  		X driver = new X();*/
 		//invoke .exe file first
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rin\\Downloads\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/C:\\Users\\Rin\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver.get("http://google.com");
 	}
 
 }
