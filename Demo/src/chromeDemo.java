365defc22fa7b715f5c3e4dabaacc823412168e9
rin1218 <kris.hyerinhyun@gmail.com>
2019-08-04 21:43:54 -0600

    Adding how to write ID through selenium. Right click on the textarea that you need and click "Inspect Element" (The code that was written by developers)

--- Demo/src/chromeDemo.java
+++ Demo/src/chromeDemo.java
@@ -1,14 +1,19 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class chromeDemo {
 	static WebDriver driver = new ChromeDriver();
 	public static void main(String[] args) {
 		// chromedriver.exe
-		System.setProperty("webdriver.chrome.driver", "/C:\\Users\\Rin\\Downloads\\seleDemo\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rin\\Downloads\\seleDemo\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://facebook.com"); //URL in the browser
+		driver.findElement(By.id("email")).sendKeys("This is my first code");
 	}
 
 }
