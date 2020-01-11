5cdf69ea16f916a5d898d539cb5038666ebddd22
Hyerin Hyun <kris.hyerinhyun@gmail.com>
2020-01-10 21:56:18 -0700

    DesiredCapability

---
+++ appPractice/base.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "RinEmulator");
+	}
+
+}
