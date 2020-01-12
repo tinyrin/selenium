d01f07b3a0e14be2fd77a09a21562e3c89902cd6
Hyerin Hyun <kris.hyerinhyun@gmail.com>
2020-01-11 19:03:11 -0700

    Add Automation name emulator

--- Tutorial/src/base.java
+++ Tutorial/src/base.java
@@ -1,19 +1,25 @@
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class base {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws MalformedURLException {
 		File f = new File("src");
 		File fs = new File(f, "Appium");
 
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "RinEmulator");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "RinEmulator");
 		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http:\\127.0.0.1:4723/wd/hub"), cap); // connect to server
+		
 	}
 
 }
