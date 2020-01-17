ec88faf0dc1edd4c44ff4c0be56c8ef17d334780
Hyerin Hyun <kris.hyerinhyun@gmail.com>
2020-01-17 12:25:48 -0700

    Order emulator to click th objects (click specific button at the last line)

--- Tutorial/src/basics.java
+++ Tutorial/src/basics.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -9,6 +10,20 @@ public class basics extends base{
 		// TODO Auto-generated method stub
 
 		AndroidDriver<AndroidElement> driver = Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//xpath id className, androidUIautomator
+		/*	xpath Syntax
+		 * 	//tagName[@attribute = 'value']
+		 * 
+		 * 	className = tagName
+		 * 
+		 */
+		
+		driver.findElementByXPath("//android.widget.TextView[@text = 'Preference']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text = '3. Preference dependencies']").click();
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
 	}
 
 }
