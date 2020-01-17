f60f477f26a888a5f0474983a17c745c2c609284
Hyerin Hyun <kris.hyerinhyun@gmail.com>
2020-01-17 11:43:21 -0700

    Setting up the basic test set up to call from anywhere

--- Tutorial/src/base.java
+++ Tutorial/src/base.java
@@ -10,15 +10,17 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class base {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
 		File f = new File("src");
 		File fs = new File(f, "ApiDemos-debug.apk");
 
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "RinEmulator");
-		//cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
 		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap); // connect to server
+		return driver;
+		//AndroidUIAutomator
 		
 	}
 
